8af37261b85802d10160141d3903ea9b3d8bd2c5
Jack Kasbeer <jkasbeer2@gmail.com>
2022-06-13 15:44:14 -0700

    shot in the dark to make mobile media queries work

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,6 @@ export default function Home() {
 
     return (
         <div id="sugoi" className="max-w-full">
-            <meta content="width=device-width, initial-scale=1" name="viewport" />
             {/* nav bar / button */}
             <div id="nav-btn">
                 {menuShown ?
