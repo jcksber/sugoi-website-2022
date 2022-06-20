3ca64e9df05d95305f2e608805ef33d1e5ec2d79
Jack Kasbeer <jkasbeer2@gmail.com>
2022-06-20 00:37:04 -0700

    kk

--- pages/securethebag.js
+++ pages/securethebag.js
@@ -1,14 +1,14 @@
 
-import { config } from "@fortawesome/fontawesome-svg-core";
-import { useState, useEffect } from 'react'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+// import { config } from "@fortawesome/fontawesome-svg-core";
+// import { useState, useEffect } from 'react'
+// import { toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
-import { ethers, utils, BigNumber } from 'ethers';
-import { useEthers } from '@usedapp/core';
+// import { ethers, utils, BigNumber } from 'ethers';
+// import { useEthers } from '@usedapp/core';
 
-config.autoAddCss = false;
+// config.autoAddCss = false;
 
-export default function Bag() {
-    
-}
+// export default function Bag() {
+
+// }
